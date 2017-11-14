924e43cecc695e92dae7f9c90bd84e6d77bbb9f2
Brandon Lee <brandonlee0405@yahoo.com>
2017-11-14 12:02:56 -0800

    commit through windows

---
+++ Smart_Parking_Lot/main.c
@@ -0,0 +1,514 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include <avr/portpins.h>
+#include <avr/pgmspace.h>
+
+//FreeRTOS include files
+#include "FreeRTOS.h"
+#include "task.h"
+#include "croutine.h"
+enum LEDState {INIT,detect} led_state;
+
+void LEDS_Init(){
+	led_state = INIT;
+}
+
+unsigned char flag = 0;
+unsigned char entrance_detected = 0;
+
+
+void LEDS_Tick(){
+	//Actions
+	unsigned char test = ~(PINB) & 0x01;
+	switch(led_state)
+	{
+		case INIT:
+		break;
+		
+		case detect:
+		break;
+		
+		default:
+		break;
+	}
+	//Transitions
+	switch(led_state)
+	{
+		case INIT:
+		led_state = detect;
+		break;
+		
+		case detect:
+		if (test)
+		{
+			flag = 1;
+			entrance_detected = 1;
+		}
+		else
+		{
+			flag = 0;
+		}
+		break;
+		
+		default:
+		break;
+	}
+}
+
+void LedSecTask()
+{
+	LEDS_Init();
+	for(;;)
+	{
+		LEDS_Tick();
+		vTaskDelay(100);
+	}
+}
+
+
+enum LightState {Light_Off,Light_On} state;
+
+void Light_Init(){
+	state = Light_Off;
+}
+
+
+void Light_Tick(){
+	//Actions
+	switch(state)
+	{
+		case Light_Off:
+		PORTD = 0x00;
+		break;
+		
+		case Light_On:
+		PORTD = 0x01;
+		break;
+		
+		default:
+		break;
+	}
+	//Transitions
+	switch(state)
+	{
+		case Light_Off:
+		if (flag == 1)
+		{
+			state = Light_On;
+		}
+		break;
+		
+		case Light_On:
+		if (flag == 0)
+		{
+			state = Light_Off;
+		}
+		break;
+		
+		default:
+		break;
+	}
+}
+
+void LightSecTask()
+{
+	Light_Init();
+	for(;;)
+	{
+		Light_Tick();
+		vTaskDelay(100);
+	}
+}
+
+enum MotorState {m_INIT,First,Second,Third,Fourth,Fifth,Sixth,Seventh,Eighth} m_state;
+unsigned short numPhases = (90/5.625) * 64;
+unsigned short phaseCounter = 0;
+unsigned short phaseCounter2 = 0;
+unsigned short openCounter;
+unsigned short waitCounter;
+
+
+void Motor_Init(){
+	m_state = m_INIT;
+}
+
+
+void Motor_Tick(){
+	//Actions
+	switch(m_state)
+	{
+		case m_INIT:
+		phaseCounter = 0;
+		phaseCounter2 = 0;
+		openCounter = 0;
+		waitCounter = 0;
+		break;
+		
+		case First:
+		PORTA = 0x01;
+		break;
+		
+		case Second:
+		PORTA = 0x03;
+		break;
+		
+		case Third:
+		PORTA = 0x02;
+		break;
+		
+		case Fourth:
+		PORTA = 0x06;
+		break;
+		
+		case Fifth:
+		PORTA = 0x04;
+		break;
+		
+		case Sixth:
+		PORTA = 0x0C;
+		break;
+		
+		case Seventh:
+		PORTA = 0x08;
+		break;
+		
+		case Eighth:
+		PORTA = 0x09;
+		break;
+		
+		default:
+		break;
+	}
+	//Transitions
+	switch(m_state)
+	{
+		case m_INIT:
+		m_state = First;
+		break;
+		
+		case First:
+		if (flag == 1)
+		{
+			if (waitCounter < 500)
+			{
+				waitCounter++;
+			}
+			else
+			{
+				if (phaseCounter <= numPhases)
+				{
+					phaseCounter++;
+					m_state = Second;
+				}
+			}
+		}
+		else
+		{
+			if (entrance_detected == 0)
+			{
+				m_state = m_INIT;
+			}
+			else
+			{
+				if (openCounter < 500)
+				{
+					openCounter++;
+				}
+				else
+				{
+					if (phaseCounter2 <= numPhases)
+					{
+						phaseCounter2++;
+						m_state = Eighth;
+					}
+					entrance_detected = 0;
+				}
+			}
+		}
+		break;
+		
+		case Second:
+		if (flag == 1)
+		{
+			if (phaseCounter <= numPhases)
+			{
+				phaseCounter++;
+				m_state = Third;
+			}
+		}
+		else
+		{
+			if (entrance_detected == 0)
+			{
+				m_state = m_INIT;
+			}
+			else
+			{
+				if (openCounter < 500)
+				{
+					openCounter++;
+				}
+				else
+				{
+					if (phaseCounter2 <= numPhases)
+					{
+						phaseCounter2++;
+						m_state = Second;
+					}
+					entrance_detected = 0;
+				}
+			}
+		}
+		break;
+		
+		case Third:
+		if (flag == 1)
+		{
+			if (phaseCounter <= numPhases)
+			{
+				phaseCounter++;
+				m_state = Fourth;
+			}
+		}
+		else
+		{
+			if (entrance_detected == 0)
+			{
+				m_state = m_INIT;
+			}
+			else
+			{
+				if (openCounter < 500)
+				{
+					openCounter++;
+				}
+				else
+				{
+					if (phaseCounter2 <= numPhases)
+					{
+						phaseCounter2++;
+						m_state = Second;
+					}
+					entrance_detected = 0;
+				}
+			}
+		}
+		break;
+		
+		case Fourth:
+		if (flag == 1)
+		{
+			if (phaseCounter <= numPhases)
+			{
+				phaseCounter++;
+				m_state = Fifth;
+			}
+		}
+		else
+		{
+			if (entrance_detected == 0)
+			{
+				m_state = m_INIT;
+			}
+			else
+			{
+				if (openCounter < 500)
+				{
+					openCounter++;
+				}
+				else
+				{
+					if (phaseCounter2 <= numPhases)
+					{
+						phaseCounter2++;
+						m_state = Third;
+					}
+					entrance_detected = 0;
+				}
+			}
+		}
+		break;
+		
+		case Fifth:
+		if (flag == 1)
+		{
+			if (phaseCounter <= numPhases)
+			{
+				phaseCounter++;
+				m_state = Sixth;
+			}
+		}
+		else
+		{
+			if (entrance_detected == 0)
+			{
+				m_state = m_INIT;
+			}
+			else
+			{
+				if (openCounter < 500)
+				{
+					openCounter++;
+				}
+				else
+				{
+					if (phaseCounter2 <= numPhases)
+					{
+						phaseCounter2++;
+						m_state = Fourth;
+					}
+					entrance_detected = 0;
+				}
+			}
+		}
+		break;
+		
+		case Sixth:
+		if (flag == 1)
+		{
+			if (phaseCounter <= numPhases)
+			{
+				phaseCounter++;
+				m_state = Seventh;
+			}
+		}
+		else
+		{
+			if (entrance_detected == 0)
+			{
+				m_state = m_INIT;
+			}
+			else
+			{
+				if (openCounter < 500)
+				{
+					openCounter++;
+				}
+				else
+				{
+					if (phaseCounter2 <= numPhases)
+					{
+						phaseCounter2++;
+						m_state = Fifth;
+					}
+					entrance_detected = 0;
+				}
+			}
+		}
+		break;
+		
+		case Seventh:
+		if (flag == 1)
+		{
+			if (phaseCounter <= numPhases)
+			{
+				phaseCounter++;
+				m_state = Eighth;
+			}
+		}
+		else
+		{
+			if (entrance_detected == 0)
+			{
+				m_state = m_INIT;
+			}
+			else
+			{
+				if (openCounter < 500)
+				{
+					openCounter++;
+				}
+				else
+				{
+					if (phaseCounter2 <= numPhases)
+					{
+						phaseCounter2++;
+						m_state = Sixth;
+					}
+					entrance_detected = 0;
+				}
+			}
+		}
+		break;
+		
+		case Eighth:
+		if (flag == 1)
+		{
+			if (phaseCounter <= numPhases)
+			{
+				phaseCounter++;
+				m_state = First;
+			}
+		}
+		else
+		{
+			if (entrance_detected == 0)
+			{
+				m_state = m_INIT;
+			}
+			else
+			{
+				if (openCounter < 500)
+				{
+					openCounter++;
+				}
+				else
+				{
+					if (phaseCounter2 <= numPhases)
+					{
+						phaseCounter2++;
+						m_state = Seventh;
+					}
+					entrance_detected = 0;
+				}
+			}
+		}
+		break;
+		
+		
+		default:
+		break;
+	}
+}
+
+void MotorSecTask()
+{
+	Motor_Init();
+	for(;;)
+	{
+		Motor_Tick();
+		vTaskDelay(3);
+	}
+}
+
+
+
+
+
+void StartSecPulse(unsigned portBASE_TYPE Priority)
+{
+	xTaskCreate(LedSecTask, (signed portCHAR *)"LedSecTask", configMINIMAL_STACK_SIZE, NULL, Priority, NULL );
+	xTaskCreate(LightSecTask, (signed portCHAR *)"LightSecTask", configMINIMAL_STACK_SIZE, NULL, Priority, NULL );
+	xTaskCreate(MotorSecTask, (signed portCHAR *)"MotorSecTask", configMINIMAL_STACK_SIZE, NULL, Priority, NULL );
+}
+
+int main(void)
+{
+	DDRB = 0x00; PORTB=0xFF;
+	DDRD = 0xFF; PORTD = 0x00;
+	DDRA = 0xFF; PORTA = 0x00;
+	
+	//Start Tasks
+	StartSecPulse(1);
+	//RunSchedular
+	vTaskStartScheduler();
+	
+	return 0;
+}
